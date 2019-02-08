16caecd9a69cbb0dd03abdb8732d64af1be689c4
Rafaela C <rafaelacutalo@gmail.com>
2019-02-08 13:33:45 -0200

    Teste 2

--- loteria.js
+++ loteria.js
@@ -1,11 +1,33 @@
-function sortear(){
-	return Math.floor(Math.random() * 60);
+var sorteados = numerosSortidos();
+var output = document.querySelector("#output");
+var msg = '';
+
+msg += '<p>Números sorteados: </p>';
+msg += '<p>' + sorteados.join('-') + '</p>';
+
+var meusNumeros = [7, 8, 10, 15, 17, 30];
+
+for (var i = 0; i < meusNumeros.length; i++) {
+	msg += '<p>' + meusNumeros[i] + ': ';
+	msg += (sorteados.indexOf(meusNumeros[i]) === -1) ? 'errou' : 'acertou';
+	msg += '</p>';
 }
+output.innerHTML = msg;
 
-var sorteados = [];
+function numerosSortidos() {
+	
+	var sorteados = [];
 
-for (var i = 0; i < 6; i++) {
-	sorteados[i] = sortear();
+	function sortear(){
+		return Math.floor(Math.random() * 60);
+	}
+
+	while(sorteados.length < 6){
+		var sorteado = sortear();
+		if (sorteados.indexOf(sorteado) === -1){
+			sorteados[sorteados.length] = sorteado;
+		}
+		console.log(sorteados.length, sorteado);
+	}
+	return sorteados;
 }
-
-console.log(sorteados);
